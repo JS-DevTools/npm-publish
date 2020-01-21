0dd9bbd6d5bd66f929912b763c52eca198fc98a5
James Messinger <james.messinger@gmail.com>
2020-01-21 07:42:18 -0600

    Removed type definition files, since they aren't needed

--- dist/npm-config.d.ts
+++
@@ -1,5 +0,0 @@
-import { Options } from "./options";
-/**
- * Sets/updates the NPM config based on the options.
- */
-export declare function setNpmConfig(options: Options): Promise<void>;
--- dist/publish-to-npm.d.ts
+++
@@ -1,6 +0,0 @@
-import { Options } from "./options";
-import { Results } from "./results";
-/**
- * Publishes an package to NPM, if its version has changed
- */
-export declare function publishToNPM(options: Options): Promise<Results>;
