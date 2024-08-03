57d570f9ead1c9bd45602e7ba0516453b7b8d2c0
Michael Cousins <michael@cousins.io>
2024-08-03 08:12:22 -0400

    fixup: fix deps and lints

--- action.js
+++ action.js
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-floating-promises, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-floating-promises, @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
 "use strict";
 
 const { main } = require("./dist/main.js");
